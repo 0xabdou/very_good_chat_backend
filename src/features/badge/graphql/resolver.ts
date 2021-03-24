6f9f9107ac8579f75eb3fc1a196c70abfb3e093a
aouahib <ouahibdou@gmail.com>
2021-03-24 17:52:11 +0100

    Add auth middleware to badge resolver

--- src/features/badge/graphql/resolver.ts
+++ src/features/badge/graphql/resolver.ts
@@ -1,19 +1,22 @@
-import {Arg, Ctx, Mutation, Query, Resolver} from "type-graphql";
+import {Arg, Ctx, Mutation, Query, Resolver, UseMiddleware} from "type-graphql";
 import {
   returnsBadge,
   returnsListOfBadges
 } from "../../../shared/graphql/return-types";
 import {Badge, BadgeName} from "./types";
 import Context from "../../../shared/context";
+import isAuthenticated from "../../auth/graphql/is-authenticated";
 
 @Resolver()
 export default class BadgeResolver {
   @Query(returnsListOfBadges)
+  @UseMiddleware(isAuthenticated)
   getBadges(@Ctx() context: Context): Promise<Badge[]> {
     return context.toolBox.dataSources.badgeDS.getUserBadges(context.userID!);
   }
 
   @Mutation(returnsBadge)
+  @UseMiddleware(isAuthenticated)
   updateBadge(@Ctx() context: Context, @Arg('badgeName', () => BadgeName) badgeName: BadgeName) {
     return context.toolBox.dataSources.badgeDS
       .updateBadge(context.userID!, badgeName);
